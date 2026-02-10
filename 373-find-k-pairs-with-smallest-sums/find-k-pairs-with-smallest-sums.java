ac237e7115a23f7438d06f9bd993cb7ca19bcacd
Srirangam Umesh Chandra <uc3932@gmail.com>
2026-02-10 15:13:38 +0530

    Time: 31 ms (62.80%) | Memory: 121 MB (68.39%) - LeetSync

---
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums1.length == 0 || nums2.length == 0 || k == 0){
+            return res;
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a,b) -> (a[0] - b[0])
+        );
+        for(int i=0;i<Math.min(k,nums1.length);i++){
+            pq.offer(new int[]{nums1[i] + nums2[0],i,0});
+        }
+        while(k -- > 0 && !pq.isEmpty()){
+            int[] curr = pq.poll();
+            int i = curr[1];
+            int j = curr[2];
+
+            res.add(Arrays.asList(nums1[i],nums2[j]));
+            if(j + 1 < nums2.length){
+                pq.offer(new int[]{
+                    nums1[i] + nums2[j+1],
+                    i,j+1
+                });
+            }
+        }
+        return res;
+    }
+}
