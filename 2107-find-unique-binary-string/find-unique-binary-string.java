fad7dc5fa1f04617a3a02f25af8285e17d405085
Srirangam Umesh Chandra <uc3932@gmail.com>
2026-03-08 09:01:22 +0530

    Time: 1 ms (59.94%) | Memory: 43.4 MB (50.14%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder res=  new StringBuilder();
+        for(int i=0;i<nums.length;i++){
+            char current = nums[i].charAt(i);
+            if(current == '0') res.append('1');
+            else res.append('0');
+        }
+        return res.toString();
+    }
+}
